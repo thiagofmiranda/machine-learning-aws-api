2b4454e10dadbf2fa7014dbd0d7a747b92749932
Thiago F. Miranda <33705632+thiagofmiranda@users.noreply.github.com>
2024-11-30 12:14:14 +0000

    fixing cors

--- lambda/src/app.py
+++ lambda/src/app.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 from mangum import Mangum
 from pydantic import BaseModel
 
 # Initialize the FastAPI app
 app = FastAPI()
-handler = Mangum(app)
+
+# Configure CORS middleware
+origins = ["*"]  
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 # Include dynamically created routes
 app.include_router(router)
+
+# Lambda handler for API Gateway integration
+handler = Mangum(app)
